6e9a5e863531f94c40e82190ad0f285533dfbfdb
Chad Parker <63312621+ChimpBite@users.noreply.github.com>
2021-01-29 11:29:41 -0700

    MVP

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
-import Login from "./components/Login";
-import "./styles.scss";
+import PrivateRoute from './components/PrivateRoute';
+import BubblePage from './components/BubblePage';
+
+import Login from './components/Login';
+import './styles.scss';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
-      </div>
-    </Router>
+    <div className='App'>
+      <Switch>
+        <PrivateRoute exact path='/bubblepage' component={BubblePage} />
+        <Redirect exact from='/bubblepage/refresh' to='/bubblepage' />
+        <Route exact path='/login' component={Login} />
+        <Route component={Login} />
+      </Switch>
+    </div>
   );
 }
 
